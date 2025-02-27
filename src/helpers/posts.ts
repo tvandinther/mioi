cddfd248a3956cee9e97b344511800b8ce1387e0
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2025-02-27 16:08:00 +0100

    Revert "try again"
    This reverts commit e3206c509c961cec28204834c540f395f974822b.

--- src/helpers/posts.ts
+++ src/helpers/posts.ts
@@ -1,5 +1,4 @@
 import { type CollectionEntry, getCollection } from "astro:content";
-import crypto from "crypto"
 
 export async function getAllPosts() {
     return await getCollection('blog')
@@ -7,11 +6,11 @@ export async function getAllPosts() {
 
 export async function getPostBanner(blogPost: CollectionEntry<"blog">) {
     const defaultBannerImages = Object.values(import.meta.glob<{default: ImageMetadata}>('public/assets/images/blog/default/*.webp'))
-    // const hashBuffer = await crypto.subtle.digest('SHA-1', new TextEncoder().encode(blogPost.id))
-    // const hashArray = new Uint8Array(hashBuffer);
-    // const num = new DataView(hashArray.buffer).getUint32(0, false); // Big-endian
-    const hash = crypto.createHash('md5').update(blogPost.id).digest()
-    const num = hash.readUInt32BE(0)
+    const hashBuffer = await crypto.subtle.digest('SHA-1', new TextEncoder().encode(blogPost.id))
+    const hashArray = new Uint8Array(hashBuffer);
+    const num = new DataView(hashArray.buffer).getUint32(0, false); // Big-endian
+    // const hash = crypto.createHash('md5').update(blogPost.id).digest()
+    // const num = hash.readUInt32BE(0)
     const getDefaultImage = defaultBannerImages[num % defaultBannerImages.length]
     const defaultImagePath = (getDefaultImage === undefined ? undefined : (await getDefaultImage()).default.src) ?? "/assets/images/blog_banner.webp"
 
