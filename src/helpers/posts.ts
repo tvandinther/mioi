b75404a4f109d7ce9ce19739aee451e7f56246aa
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2025-02-27 15:42:24 +0100

    add logic for using deterministically random of default images

--- src/helpers/posts.ts
+++ src/helpers/posts.ts
@@ -1,7 +1,17 @@
 import { type CollectionEntry, getCollection } from "astro:content";
+import crypto from "crypto"
 
 export async function getAllPosts() {
     return await getCollection('blog')
+}
+
+export async function getPostBanner(blogPost: CollectionEntry<"blog">) {
+    const defaultBannerImages = Object.values(import.meta.glob<{default: ImageMetadata}>('public/assets/images/blog/default/*.webp'))
+    const hash = crypto.createHash('md5').update(blogPost.id).digest()
+    const getDefaultImage = defaultBannerImages[hash.readUInt32BE(0) % defaultBannerImages.length]
+    const defaultImagePath = (getDefaultImage === undefined ? undefined : (await getDefaultImage()).default.src) ?? "/assets/images/blog_banner.webp"
+
+    return defaultImagePath
 }
 
 export const byLatest = (a: CollectionEntry<"blog">, b: CollectionEntry<"blog">) => a.data.date < b.data.date ? 1 : -1;
